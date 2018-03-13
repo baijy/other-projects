ca9ce29ab4d9e8babe9e4464bcd67ea402f69d6d
baijy <0755bai@gmail.com>
2018-03-13 23:16:18 +0800

    after service OK

--- dubbo-order/order-provider/src/main/java/com/jianyu/dubbo/order/App.java
+++ dubbo-order/order-provider/src/main/java/com/jianyu/dubbo/order/App.java
@@ -2,6 +2,9 @@ package com.jianyu.dubbo.order;
 
 import com.alibaba.dubbo.container.Main;
 
+/**
+ * Dubbo 服务需要在这里启动，用容器启动是没有作用的
+ */
 public class App {
     public static void main(String[] args) {
         Main.main(args);
--- dubbo-user/src/main/java/com/jianyu/dubbo/user/App.java
+++ dubbo-user/src/main/java/com/jianyu/dubbo/user/App.java
@@ -1,13 +1,25 @@
 package com.jianyu.dubbo.user;
 
+import com.jianyu.dubbo.order.DoOrderRequest;
+import com.jianyu.dubbo.order.DoOrderResponse;
+import com.jianyu.dubbo.order.IOrderServices;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context  = new ClassPathXmlApplicationContext("order-consumer.xml");
 
+        IOrderServices services = (IOrderServices) context.getBean("orderServices");
 
+        DoOrderRequest request = new DoOrderRequest();
+        request.setName("包子哥哥");
+        DoOrderResponse response = services.doOrder(request);
 
+        System.out.println(response);
+
+        System.in.read();
 
     }
 }
